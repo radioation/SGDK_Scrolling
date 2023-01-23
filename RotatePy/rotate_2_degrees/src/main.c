d09af327dacc5b22523cc23bd3ba1448182a005d
Greg Gallardo <greggallardo@gmail.com>
2023-01-22 21:19:07 -0600

    2 degree example

---
+++ RotatePy/rotate_2_degrees/src/main.c
@@ -0,0 +1,44 @@
+#include <genesis.h>
+#include "resources.h"
+#include "rotation.h"
+
+/////////////////////////////////////////////////////////////////////
+// Scrolling Stuff
+#define PLANE_MAX_TILE 64
+
+int main(bool hard)
+{
+  VDP_setScreenWidth320();
+  // set colors
+  PAL_setPalette( PAL0, platform_pal.data, CPU );
+
+  // set scrolling mode to LINE for horizontal and TILE for vertical
+  VDP_setScrollingMode(HSCROLL_LINE, VSCROLL_2TILE);
+
+  // get tile positions in VRAM.
+  int ind = TILE_USER_INDEX;
+  int indexA = ind;
+  // Load the plane tiles into VRAM
+  VDP_loadTileSet(platform.tileset, ind, DMA);
+
+  // setup the tiles
+  VDP_setTileMapEx(BG_A, platform.tilemap, TILE_ATTR_FULL(PAL0, TRUE, FALSE, FALSE, indexA),
+                   0,               // Plane X destination
+                   0,               // plane Y destination
+                   0,               // Region X start position
+                   0,               // Region Y start position
+                   PLANE_MAX_TILE, // width  (went with 64 becasue default width is 64.  Viewable screen is 40)
+                   28,             // height
+                   CPU);
+
+  // set SGDK scrolling functions to fake the rotaiton.
+  VDP_setHorizontalScrollLine(BG_A, START_ROW_A, _hScroll, ROWS_A, CPU);
+  VDP_setVerticalScrollTile(BG_A, START_COL_A, _vScroll, COLS_A, CPU);
+
+  while (TRUE)
+  {
+    // let SGDK do its thing
+    SYS_doVBlankProcess();
+  }
+  return 0;
+}
---
+++ RotatePy/rotate_2_degrees/src/rotation.h
@@ -0,0 +1,25 @@
+#ifndef _ROTATION_H_
+#define _ROTATION_H_
+
+
+#define _SCROLL_COUNT 1
+#define ROWS_A 21
+#define START_ROW_A 93
+#define END_ROW_A 113
+#define COLS_A 20
+#define START_COL_A 0
+#define END_COL_A 19
+
+
+s16 _hScroll[] = {
+  // rotation values for angle 2.000000 starts at 0
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+
+s16 _vScroll[] = {
+ // rotation values for angle 2.000000 starts at 0
+-5, -4, -4, -3, -3, -2, -2, -1, -1, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 6
+};
+
+
+#endif // _ROTATION_H_
