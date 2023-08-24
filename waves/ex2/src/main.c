1d6bb19715f754171030c8e64f6fc9ffdb22d8a2
Greg Gallardo <greggallardo@gmail.com>
2023-08-23 22:33:01 -0500

    working on horizontal offsets

--- waves/ex2/src/main.c
+++ waves/ex2/src/main.c
@@ -25,72 +25,57 @@ int main(bool hard) {
   // use LINE scroll for horizontal 
   VDP_setScrollingMode( HSCROLL_LINE, VSCROLL_PLANE);
 
-  s16 hScroll[TOTAL_LINES]; 
-  memset( hScroll, 0, sizeof(hScroll));
-  fix16 offsetPlaneSky = FIX16(0);
-  fix16 landOffsets[TOTAL_LINES];
-  memset( landOffsets, 0, sizeof(landOffsets));
+  // background
+  s16 hScrollB[TOTAL_LINES]; 
+  memset( hScrollB, 0, sizeof(hScrollB));
 
 
   u16 sinPerLine = 5;    // Elements to jump in sin() per line. Larger values give us faster waves.
   fix16 amplitude = FIX16( 10.0 );  // Amplitude sets how big the waves are.
-  s16 offset = -40;  // shift left a bit.
-  for( u16 i = 0; i < TOTAL_LINES; ++i ) {
-    hScroll[i] = fix16ToInt( fix16Mul(  sinFix16(i * sinPerLine ), amplitude ) ) + offset;
-  }
+  s16 offsetB = -40;  // shift left a bit.
 
+
+  // foreground
+  s16 hScrollA[TOTAL_LINES]; 
+  s16 offsetA = 0;  // shift left a bit.
+  s16 aDir = -1;
+  memset( hScrollA, 0, sizeof(hScrollA));
 
   //////////////////////////////////////////////////////////////
   // main loop.
   u16 sinOffset = 0; // Step basically tells us where we're starting in the sin table.
   while(TRUE)
   {
-    // read joypad to set sinewave parameters 
+    // read joypad to set horizontal scrolling positions.
     u16 joypad  = JOY_readJoypad( JOY_1 );
-    if( joypad & BUTTON_UP ) {
-      sinPerLine++; 
-    } else if( joypad & BUTTON_DOWN ) {
-      sinPerLine--; 
-    }
     if( joypad & BUTTON_LEFT ) {
-      amplitude = fix16Sub( amplitude, FIX16(1));
+      offsetA +=2;
+        if ( offsetA > 0 ) {
+            offsetA = 0;
+        }
     } else if( joypad & BUTTON_RIGHT ) {
-      amplitude = fix16Add( amplitude, FIX16(1));
+      offsetA -=2;
+        if ( offsetA < -158 ) {
+            offsetA = -158;
+        }
     }
 
-    if( joypad & BUTTON_A) {
-      sinPerLine = 5;    
-      amplitude = FIX16( 10.0 );
-    } 
-    if( joypad & BUTTON_B) {
-      sinPerLine = 10;    
-      amplitude = FIX16( 30.0 );
-    } 
-    if( joypad & BUTTON_C) {
-      sinPerLine = 80;     
-      amplitude = FIX16( 0.5 );
-    } 
-
-    // write params to the screen.
-    char message[40];
-    char amps[5];
-    fix16ToStr( amplitude, amps, 1 );
-    strclr(message);
-    sprintf( message, "sin per line: %d amplitude: %s  ", sinPerLine, amps );
-    VDP_drawText(message, 3, 1 );
 
     //////////////////////////////////////////////////////////////
-    // This is what matters right here:  
+    // Wavy scrolling
     //    calculate the offsets per line using SGDK's sin table
     //    and adjust with params
     sinOffset++; // move up in the sine table
     for( u16 i = 0; i < TOTAL_LINES; ++i ) {
         // compute horizontal offsets with sine table.
-        hScroll[i] = fix16ToInt( fix16Mul(  sinFix16(( i + sinOffset ) * sinPerLine ), amplitude ) ) + offset;
+        hScrollB[i] = fix16ToInt( fix16Mul(  sinFix16(( i + sinOffset ) * sinPerLine ), amplitude ) ) + offsetB;
+        hScrollA[i] = offsetA;
     }
 
+   
     // apply scrolling offsets 
-    VDP_setHorizontalScrollLine (BG_B, 0, hScroll, 223, DMA_QUEUE);
+    VDP_setHorizontalScrollLine (BG_B, 0, hScrollB, 223, DMA_QUEUE);
+    VDP_setHorizontalScrollLine (BG_A, 0, hScrollA, 223, DMA_QUEUE);
     SYS_doVBlankProcess();
 
   }
