95a16bf57bde6d97f90da4b14948328301554d8f
Greg Gallardo <greggallardo@gmail.com>
2023-08-23 21:38:15 -0500

    basic sinewave example working

--- waves/ex1/src/main.c
+++ waves/ex1/src/main.c
@@ -29,26 +29,64 @@ int main(bool hard) {
   memset( landOffsets, 0, sizeof(landOffsets));
 
 
-  u16 rads = 5; // radians per step.   
-  fix16 amplitude = FIX16( 20.0 );
+  u16 sinPerLine = 5;    // Elements to jump in sin() per line. Larger values give us faster waves.
+  fix16 amplitude = FIX16( 10.0 );  // Amplitude sets how big the waves are.
   s16 offset = -40;  // shift left a bit.
   for( u16 i = 0; i < TOTAL_LINES; ++i ) {
-    hScroll[i] = fix16ToInt( fix16Mul(  sinFix16(i * rads ), amplitude ) ) + offset;
+    hScroll[i] = fix16ToInt( fix16Mul(  sinFix16(i * sinPerLine ), amplitude ) ) + offset;
   }
 
 
   //////////////////////////////////////////////////////////////
   // main loop.
-  u16 step = 0;
+  u16 sinOffset = 0; // Step basically tells us where we're starting in the sin table.
   while(TRUE)
   {
-    // calculate the offsets using SGDK's sin table.
-    step++;
-    for( u16 i = 0; i < TOTAL_LINES; ++i ) {
-      hScroll[i] = fix16ToInt( fix16Mul(  sinFix16(( i + step ) * rads ), amplitude ) ) + offset;
+    // read joypad to set sinewave parameters 
+    u16 joypad  = JOY_readJoypad( JOY_1 );
+    if( joypad & BUTTON_UP ) {
+      sinPerLine++; 
+    } else if( joypad & BUTTON_DOWN ) {
+      sinPerLine--; 
+    }
+    if( joypad & BUTTON_LEFT ) {
+      amplitude = fix16Sub( amplitude, FIX16(1));
+    } else if( joypad & BUTTON_RIGHT ) {
+      amplitude = fix16Add( amplitude, FIX16(1));
     }
 
-    // apply scrolling
+    if( joypad & BUTTON_A) {
+      sinPerLine = 5;    
+      amplitude = FIX16( 10.0 );
+    } 
+    if( joypad & BUTTON_B) {
+      sinPerLine = 10;    
+      amplitude = FIX16( 30.0 );
+    } 
+    if( joypad & BUTTON_C) {
+      sinPerLine = 80;     
+      amplitude = FIX16( 0.5 );
+    } 
+
+    // write params to the screen.
+    char message[40];
+    char amps[5];
+    fix16ToStr( amplitude, amps, 1 );
+    strclr(message);
+    sprintf( message, "sin per line: %d amplitude: %s  ", sinPerLine, amps );
+    VDP_drawText(message, 3, 1 );
+
+    //////////////////////////////////////////////////////////////
+    // This is what matters right here:  
+    //    calculate the offsets per line using SGDK's sin table
+    //    and adjust with params
+    sinOffset++; // move up in the sine table
+    for( u16 i = 0; i < TOTAL_LINES; ++i ) {
+        // compute horizontal offsets with sine table.
+        hScroll[i] = fix16ToInt( fix16Mul(  sinFix16(( i + sinOffset ) * sinPerLine ), amplitude ) ) + offset;
+    }
+
+    // apply scrolling offsets 
     VDP_setHorizontalScrollLine (BG_B, 0, hScroll, 223, DMA_QUEUE);
     SYS_doVBlankProcess();
 
