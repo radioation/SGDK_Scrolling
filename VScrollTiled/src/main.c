d11b69d094f77eb497c10b9a4cb96968e5b84127
radioation <greggallardo@gmail.com>
2024-12-01 13:42:13 -0600

    Compile fixes for SGDK 2.0

--- VScrollTiled/src/main.c
+++ VScrollTiled/src/main.c
@@ -27,7 +27,7 @@ int main(bool hard)
   PAL_setColor(0, 0x0000);
 
   // set scrolling modes to support fake rotation
-  VDP_setScrollingMode(HSCROLL_PLANE, VSCROLL_2TILE);
+  VDP_setScrollingMode(HSCROLL_PLANE, VSCROLL_COLUMN);
 
   // get initial tile position in VRAM
   int ind = TILE_USER_INDEX;
